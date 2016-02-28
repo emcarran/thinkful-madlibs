02779e746d9385ae6b3b63d76942f86768ab7eec
Marius Banea <marius@w-d-s.co.uk>
2016-02-28 02:41:09 +0000

    updated logic and look and feel

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 var app = angular.module('ngMadLibs', ['ngAnimate'])
 
-app.constant('VERSION', "4")
-app.run(function(VERSION, $rootScope) {
+app.constant('VERSION', "5")
+app.run(function (VERSION, $rootScope) {
     $rootScope.version = VERSION;
 })
 
-app.controller('madLibsController',function($scope){
+app.controller('madLibsController', function ($scope) {
 
     $scope.data = {};
 
-    $scope.gender = {
-        pronoun: 'she',
-        poss: 'her'
-    };
+    //the gender values will populated here by the input values in the index.html
+    $scope.gender = {};
 
+    //by default do not hide the form
     $scope.hideForm = false;
 
-    $scope.submit = function(){
+    $scope.submit = function () {
+        //hide the form only after submit
         $scope.hideForm = true;
     }
 
-    $scope.reset = function(){
+    $scope.reset = function () {
         $scope.data = {};
         $scope.hideForm = false;
         $scope.submitted = false;
